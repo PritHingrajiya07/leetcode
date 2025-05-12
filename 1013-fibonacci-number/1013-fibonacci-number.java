0075eb111cafdedd50cd56abbd62d6b8aedcb485
Prit Hingrajiya <162274404+PritHingrajiya07@users.noreply.github.com>
2025-05-12 13:37:32 +0530

    Time: 8 ms (43.4%), Space: 40.2 MB (90.23%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,16 +1,20 @@
 class Solution {
     public int fib(int n) {
 
-        int F=1;
-        int S = 1;
+        // int F=1;
+        // int S = 1;
 
-        for(int i=1;i<=n;i++){
-            int ans = F+S;
+        // for(int i=1;i<=n;i++){
+        //     int ans = F+S;
 
-            F = S;
-            S =ans;
-        }
-        return ( S-F );
+        //     F = S;
+        //     S =ans;
+        // }
+        // return ( S-F );
+
+
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
     } 
     
 }
