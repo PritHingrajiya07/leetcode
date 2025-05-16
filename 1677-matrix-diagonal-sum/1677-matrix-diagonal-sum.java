44f4e1f2e5a10019d772ba62ee9ae43d26c88431
Prit Hingrajiya <162274404+PritHingrajiya07@users.noreply.github.com>
2025-05-16 10:41:26 +0530

    Time: 0 ms (100%), Space: 44.4 MB (60.3%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -8,8 +8,7 @@ class Solution {
             sum += mat[i][i];
             sum += mat[i][l-1-i];           
         }
-        if(l%2 == 1)    sum-=mat[l/2][l/2];
-
+        if(l%2 != 0)    sum-=mat[l/2][l/2];
         return sum;
         
     }
