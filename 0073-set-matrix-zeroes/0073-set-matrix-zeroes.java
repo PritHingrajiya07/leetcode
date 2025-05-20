907f22e7b032f4174697fa38aba23ab6afd0233e
Prit Hingrajiya <162274404+PritHingrajiya07@users.noreply.github.com>
2025-05-20 11:30:33 +0530

    Time: 1 ms (72.87%), Space: 45.6 MB (66.49%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,14 +1,17 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        
+        // Taking dimension of original matrix
         int row = matrix.length;        
         int col = matrix[0].length;
+        // Creating duplicate matrix to make changes named ans
         int[][] ans = new int[row][col];
 
+        // Duplicating all original elements in ans[][]
         for(int i=0; i<row; i++)
             for(int j=0; j<col; j++)
                 ans[i][j] = matrix[i][j];
 
+        // Finding 0 in original matrix and making changes in ans matrix for safe traversal in org matrix
         for(int i=0; i<row; i++)
             for(int j=0; j<col; j++)
                 if(matrix[i][j] == 0){
@@ -16,6 +19,7 @@ class Solution {
                     for(int rw=0; rw<row; rw++) ans[rw][j] =0;
                 }
 
+        // Copying back ans in original matrix as it is required according to problem statement and void returnType
         for(int i=0; i<row; i++)
             for(int j=0; j<col; j++)
                 matrix[i][j] = ans[i][j];
